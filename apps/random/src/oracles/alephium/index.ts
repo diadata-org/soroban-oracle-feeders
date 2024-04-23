f66827d626c13b12021ced5008d12b989c01349f
nkorn1lov <nikolai.kornilov@protofire.io>
2024-04-24 01:01:52 +0300

    fix: randomness oracle encoding

--- apps/random/src/oracles/alephium/index.ts
+++ apps/random/src/oracles/alephium/index.ts
@@ -1,8 +1,8 @@
-import { NodeProvider, stringToHex, ONE_ALPH, web3 } from '@alephium/web3'
-import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { NodeProvider, web3 } from '@alephium/web3';
+import { PrivateKeyWallet } from '@alephium/web3-wallet';
 
-import { DIARandomOracle, SetRandomValue } from './artifacts/ts'
-import config, {ChainName} from '../../config';
+import { DIARandomOracle, SetRandomValue } from './artifacts/ts';
+import config, { ChainName } from '../../config';
 import type { DrandResponse } from '../../api';
 
 let nodeProvider: NodeProvider;
@@ -13,33 +13,37 @@ if (config.chainName === ChainName.ALEPHIUM) {
 }
 
 export function init() {
-  nodeProvider = new NodeProvider(config.alephium.rpcUrl)
-  web3.setCurrentNodeProvider(nodeProvider)
-  wallet = new PrivateKeyWallet({privateKey: config.alephium.secretKey, keyType: undefined, nodeProvider: nodeProvider})
+  nodeProvider = new NodeProvider(config.alephium.rpcUrl);
+  web3.setCurrentNodeProvider(nodeProvider);
+  wallet = new PrivateKeyWallet({
+    privateKey: config.alephium.secretKey,
+    keyType: undefined,
+    nodeProvider: nodeProvider,
+  });
 }
 
 export const getLastRound = async () => {
-  const diaRandomOracle = await DIARandomOracle.at(config.alephium.contract)
-  const round = (await diaRandomOracle.methods.getLastRound()).returns
-  return Number(round)
-}
+  const diaRandomOracle = DIARandomOracle.at(config.alephium.contract);
+  const round = (await diaRandomOracle.methods.getLastRound()).returns;
+  return Number(round);
+};
 
 export async function updateOracle(data: DrandResponse) {
-  const diaRandomOracle = await DIARandomOracle.at(config.alephium.contract)
-  const subContractDeposit = (await diaRandomOracle.methods.getSubContractDeposit()).returns
-  
+  const diaRandomOracle = DIARandomOracle.at(config.alephium.contract);
+  const subContractDeposit = (await diaRandomOracle.methods.getSubContractDeposit()).returns;
+
   const result = await SetRandomValue.execute(wallet, {
     initialFields: {
       round: BigInt(data.round),
       value: {
-        randomness: stringToHex(data.randomness),
-        signature: stringToHex(data.signature),
-        previousSignature: stringToHex(data.previous_signature),
+        randomness: data.randomness,
+        signature: data.signature,
+        previousSignature: data.previous_signature,
       },
       oracle: diaRandomOracle.contractId,
     },
-    attoAlphAmount: subContractDeposit
-  })
-  console.log('result:', result)
+    attoAlphAmount: subContractDeposit,
+  });
+  console.log('result:', result);
   console.log('Random oracle updated');
 }
